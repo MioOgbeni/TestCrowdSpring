15036553e41207f78462d9b6b97fbf1695c6f86a
Libor Plisek <liborplisek@gmail.com>
2020-01-13 17:41:57 +0100

    TestCrowd 1.0.0 | Add OAuth2 login + Edit dependencies in pom.xml + Rename application.yaml to application.yml + Add configurations to application.yml + SQL backup

--- src/main/java/cz/spitsoft/testcrowd/TestcrowdApplication.java
+++ src/main/java/cz/spitsoft/testcrowd/TestcrowdApplication.java
@@ -3,8 +3,12 @@ package cz.spitsoft.testcrowd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication(proxyBeanMethods = false, exclude = {SecurityAutoConfiguration.class})
+@EnableWebSecurity
+@Configuration
 public class TestcrowdApplication {
 
     public static void main(String[] args) {
