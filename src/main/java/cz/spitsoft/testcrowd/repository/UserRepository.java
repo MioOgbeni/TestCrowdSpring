0f92b806932c2e774a0de4f1720b35e4d3b4e78f
Libor Plisek <liborplisek@gmail.com>
2020-01-16 21:11:56 +0100

    TestCrowd 1.0.0 | Add user repository + Add new user model + Add user repository

--- src/main/java/cz/spitsoft/testcrowd/repository/UserRepository.java
+++ src/main/java/cz/spitsoft/testcrowd/repository/UserRepository.java
@@ -1,14 +1,14 @@
 package cz.spitsoft.testcrowd.repository;
 
-import cz.spitsoft.testcrowd.model.User;
+import cz.spitsoft.testcrowd.model.User2;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-//@Repository
-//public interface UserRepository extends JpaRepository<User, Long> {
-    //Optional<User> findByEmail(String email);
+@Repository
+public interface UserRepository extends JpaRepository<User2, Long> {
+    Optional<User2> findByEmail(String email);
 
-    //Boolean existsByEmail(String email);
-//}
+    Boolean existsByEmail(String email);
+}
