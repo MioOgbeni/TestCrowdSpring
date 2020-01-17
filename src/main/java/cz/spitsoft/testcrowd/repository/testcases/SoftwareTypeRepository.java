dd97041b20fbb63d974d56d9a2f51964db3c64bc
Tomáš Novák <tom.nov96@gmail.com>
2020-01-17 18:46:24 +0100

    TestCrowd 1.0.0 | SoftwareType and TestCategory REPO
    + SoftwareTypeRepository (done)
    + TestCategoryRepository (done)

---
+++ src/main/java/cz/spitsoft/testcrowd/repository/testcases/SoftwareTypeRepository.java
@@ -0,0 +1,26 @@
+package cz.spitsoft.testcrowd.repository.testcases;
+
+import cz.spitsoft.testcrowd.model.UserImp;
+import cz.spitsoft.testcrowd.model.testcases.SoftwareTypeImp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface SoftwareTypeRepository extends JpaRepository<SoftwareTypeImp, UUID> {
+
+    @Transactional(readOnly = true)
+    List<SoftwareTypeImp> findByValidTrue();
+
+    @Transactional(readOnly = true)
+    List<SoftwareTypeImp> findByValidFalse();
+
+    @Transactional(readOnly = true)
+    SoftwareTypeImp findByName();
+
+    @Transactional(readOnly = true)
+    List<SoftwareTypeImp> findByModifiedBy(UserImp user);
+}
---
+++ src/main/java/cz/spitsoft/testcrowd/repository/testcases/TestCategoryRepository.java
@@ -0,0 +1,27 @@
+package cz.spitsoft.testcrowd.repository.testcases;
+
+import cz.spitsoft.testcrowd.model.UserImp;
+import cz.spitsoft.testcrowd.model.testcases.SoftwareTypeImp;
+import cz.spitsoft.testcrowd.model.testcases.TestCategoryImp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface TestCategoryRepository extends JpaRepository<TestCategoryImp, UUID> {
+
+    @Transactional(readOnly = true)
+    List<SoftwareTypeImp> findByValidTrue();
+
+    @Transactional(readOnly = true)
+    List<SoftwareTypeImp> findByValidFalse();
+
+    @Transactional(readOnly = true)
+    SoftwareTypeImp findByName();
+
+    @Transactional(readOnly = true)
+    List<SoftwareTypeImp> findByModifiedBy(UserImp user);
+}
