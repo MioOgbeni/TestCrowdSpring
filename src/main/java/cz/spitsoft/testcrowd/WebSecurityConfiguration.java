a4f60d6f611d46db0b66cffb1db8d8d2b14b5178
Libor Plisek <liborplisek@gmail.com>
2020-01-19 20:31:23 +0100

    TestCrowd 1.0.0 | Edit web security config file + Edit web security config file

--- src/main/java/cz/spitsoft/testcrowd/WebSecurityConfiguration.java
+++ src/main/java/cz/spitsoft/testcrowd/WebSecurityConfiguration.java
@@ -28,7 +28,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/resources/**", "/registration", "/", "/welcome", "/index").permitAll()
+                .antMatchers("/contrib/**", "/custom/**", "/registration", "/", "/welcome", "/index").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
