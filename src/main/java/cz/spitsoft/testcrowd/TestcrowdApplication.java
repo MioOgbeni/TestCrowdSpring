e3cf3ff5b214b3b2ed96d9a7a94f1cce3038e2fa
Tomáš Novák <tom.nov96@gmail.com>
2020-01-19 02:53:28 +0100

    TestCrowd 1.0.0 | Custom error handling v1 (in progress)
    + ADDED MyErrorController (in progress)
    + ADDED error 404 and 500 templates (in progress)

--- src/main/java/cz/spitsoft/testcrowd/TestcrowdApplication.java
+++ src/main/java/cz/spitsoft/testcrowd/TestcrowdApplication.java
@@ -3,7 +3,7 @@ package cz.spitsoft.testcrowd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class TestcrowdApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestcrowdApplication.class, args);
