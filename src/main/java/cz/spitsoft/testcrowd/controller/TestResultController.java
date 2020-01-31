c38f7de281aaeb947658a128fc59dea9aa14d864
Libor Plisek <liborplisek@gmail.com>
2020-01-31 01:18:01 +0100

    TestCrowd 1.0.0 | Edit test result + Edit test result controller

--- src/main/java/cz/spitsoft/testcrowd/controller/TestResultController.java
+++ src/main/java/cz/spitsoft/testcrowd/controller/TestResultController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.Date;
 
 @Controller
-public class ResultController {
+public class TestResultController {
 
     @Autowired
     private SecurityService securityService;
