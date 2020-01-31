e8b6c98aa953487204d155cd1c761dfa2ec9509d
Libor Plisek <liborplisek@gmail.com>
2020-01-31 17:34:57 +0100

    TestCrowd 1.0.0 | Edit templates and styles + Edit templates and styles + Sync SQL backup files

--- src/main/java/cz/spitsoft/testcrowd/model/test_case/TestResultStatus.java
+++ src/main/java/cz/spitsoft/testcrowd/model/test_case/TestResultStatus.java
@@ -3,5 +3,4 @@ package cz.spitsoft.testcrowd.model.test_case;
 public enum TestResultStatus {
     TAKEN,
     DONE,
-    REVIEWED,
 }
--- src/main/java/cz/spitsoft/testcrowd/model/test_case/TestStatus.java
+++ src/main/java/cz/spitsoft/testcrowd/model/test_case/TestStatus.java
@@ -2,7 +2,5 @@ package cz.spitsoft.testcrowd.model.test_case;
 
 public enum TestStatus {
     AVAILABLE, // test is available for take
-    TAKEN, // test is taken (test can not be taken)
-    DONE, // test is done (test can not be taken)
     CANCELED // test can not be taken
 }
